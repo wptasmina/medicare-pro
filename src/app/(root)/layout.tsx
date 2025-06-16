bfde53d2b9606edbc6f0343e0e2d9b8fd713c566
wptasmina <wptasmina@gmail.com>
2025-06-17 02:37:18 +0600

    footer section add

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,16 +1,17 @@
-import { ReactNode } from "react";
+
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer/Footer";
 
-interface LayoutProps {
-  children: ReactNode;
-}
+// interface LayoutProps {
+//   children: ReactNode;
+// }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
       <main>{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
