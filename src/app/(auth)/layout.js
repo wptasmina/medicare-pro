bcbbd42a39e31218d32507db7fa316640eded42f
wptasmina <wptasmina@gmail.com>
2025-06-17 10:45:58 +0600

    folder structure

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,11 +1,12 @@
 import Navbar from "@/components/Navbar";
+import { Footer } from '@/components/footer/Footer';
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
         <main>{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
