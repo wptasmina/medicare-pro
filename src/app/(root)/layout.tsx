047d790d2c010320e472086305474e397f5bd17f
wptasmina <wptasmina@gmail.com>
2025-06-19 00:36:37 +0600

    add navbar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,9 @@
-// import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <main>{children}</main>
     </>
   );
